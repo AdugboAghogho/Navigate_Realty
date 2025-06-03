ba79a900a4326ca61122a9590ad9804df2491c18
adugbo aghogho <adugboaghogho@gmail.com>
2025-06-03 12:02:48 +0100

    starting

---
+++ Navigate_Realty/app/(root)/_layout.tsx
@@ -0,0 +1,17 @@
+// import { Redirect, Slot } from "expo-router";
+// import { ActivityIndicator } from "react-native";
+// import { SafeAreaView } from "react-native-safe-area-context";
+
+// // import { useGlobalContext } from "@/lib/global-provider";
+
+// export default function AppLayout() {
+//   // const { loading, isLogged } = useGlobalContext();
+
+//   return (
+//     <SafeAreaView className="bg-white h-full flex justify-center items-center">
+//       {/* <ActivityIndicator className="text-primary-300" size="large" /> */}
+//     </SafeAreaView>
+//   );
+
+//   // return <Slot />;
+// }
